748f540a69bb7d502fc417d99054052e2dda30cd
Yassir mASTERRcpp <dj.jicara@gmail.com>
2022-06-12 17:53:50 -0500

    Se ha corregido el error de los acentos del nombre de usuario

--- saveinfousuario.js
+++ saveinfousuario.js
@@ -1,15 +1,17 @@
     
+    //la siguiente función fue actualizada completamente para solucionar el problema de los acentos        
+    // fuente: https://concamilo.com/como-obtener-los-parametros-de-la-url-con-javascript/
+
+    
     function getQueryVariable(variable) { //para obtener algún valor que se guardó en el url a través del método GET de un formulario
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        //alert(vars);
-        for (var i=0; i < vars.length; i++) {
-            var pair = vars[i].split("="); 
-            if (pair[0] == variable) {
-                return pair[1];
-            }
-        }
-        return false;
+      var valores = window.location.search;                                                      
+                
+      //ALMACENAMOSEN VARIABLES 
+      //Creamos la instancia
+      var urlParams = new URLSearchParams(valores); //js permite
+
+      //Accedemos a los valores
+      return urlParams.get(variable);
     }
 
 //alert(localStorage.getItem('unidad'));
